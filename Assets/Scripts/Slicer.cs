68e2118c4f8cf4317a9a084895e1b29a6bc3502a
WengerM996 <maks.wenger@yandex.ru>
2021-03-03 12:52:12 +0300

    init

---
+++ Assets/Scripts/Slicer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BzKovSoft.ObjectSlicerSamples;
+using DG.Tweening;
+using UnityEngine;
+
+public class Slicer : MonoBehaviour
+{
+    [SerializeField] private GameObject _blade;
+    [SerializeField] private float _duration;
+    [SerializeField] private float _offsetY;
+
+    private BzKnife _knife;
+
+    private void Start()
+    {
+        _knife = _blade.GetComponentInChildren<BzKnife>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _knife.BeginNewSlice();
+
+            _blade.transform.DOMoveY(_blade.transform.position.y - _offsetY, _duration).SetLoops(2, LoopType.Yoyo);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _templates;
+    [SerializeField] private float _spawnDelay;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        while (true)
+        {
+            var foodNumber = Random.Range(0, _templates.Length);
+            Instantiate(_templates[foodNumber], transform.position, _templates[foodNumber].transform.rotation);
+            yield return new WaitForSeconds(_spawnDelay);
+        }
+    }
+}
